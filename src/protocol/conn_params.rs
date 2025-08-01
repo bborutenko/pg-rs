3931d890923bbf75f7628ef79211639910c796ea
bborutenko <bborutenko@gmail.com>
2025-08-01 16:21:37 +0300

    write conn_params module

--- src/protocol/conn_params.rs
+++ src/protocol/conn_params.rs
@@ -1,12 +1,11 @@
 use std::net::Ipv4Addr;
 
-
-pub struct ConnParams<'a> {
-    user: &'a str,
-    password: Option<&'a str>,
+pub struct ConnParams {
+    user: String,
+    password: Option<String>,
     host: Ipv4Addr,
     port: u32,
-    database: &'a str,
+    database: String,
 }
 
 pub struct ConnParamsBuilder<'a> {
@@ -18,13 +17,58 @@ pub struct ConnParamsBuilder<'a> {
 
 }
 
-// todo: impl for conn and unit test
+impl ConnParams {
+    #[inline]
+    pub fn user(&self) -> &String {
+        &self.user
+    }
+
+    #[inline]
+    pub fn password(&self) -> Option<&String> {
+        self.password.as_ref()
+    }
+
+    #[inline]
+    pub fn host(&self) -> &Ipv4Addr {
+        &self.host
+    }
+
+    #[inline]
+    pub fn port(&self) -> &u32 {
+        &self.port
+    }
+
+    #[inline]
+    pub fn database(&self) -> &String {
+        &self.database
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut url = format!("postgresql://{}", self.user);
+
+        url = match &self.password {
+            Some(pass) => format!("{}:{}", url, pass),
+            None => url,
+        };
+
+        url = format!(
+            "{}@{}:{}/{}",
+            url,
+            self.host.to_string(),
+            self.port.to_string(),
+            self.database,
+        );
+
+        url
+    }
+}
+
 
 impl<'a> ConnParamsBuilder<'a> {
     pub fn new() -> ConnParamsBuilder<'a> {
         ConnParamsBuilder { 
             user: "postgres", 
-            password: None, 
+            password: None,
             host: Ipv4Addr::new(127, 0, 0, 1), // localhost 
             port: 5432, 
             database: "postgres", 
@@ -39,9 +83,9 @@ impl<'a> ConnParamsBuilder<'a> {
     }
 
     pub fn password(
-        &'a mut self, password: Option<&'a str>,
+        &'a mut self, password: &'a str,
     ) -> &'a mut ConnParamsBuilder<'a> {
-        self.password = password;
+        self.password = Some(password);
         self
     }
 
@@ -66,18 +110,19 @@ impl<'a> ConnParamsBuilder<'a> {
         self
     }
 
-    pub fn build(&'a self) -> ConnParams<'a> {
-        ConnParams{
-            user: self.user, 
-            password: self.password, 
+    pub fn build(&'a self) -> ConnParams {
+        ConnParams {
+            user: self.user.to_string(), 
+            password: match self.password {
+                Some(pass) => Some(pass.to_string()),
+                None => None,
+            }, 
             host: self.host, 
             port: self.port, 
-            database: self.database, 
-
+            database: self.database.to_string(), 
         }
     }
 }
-
 
 #[cfg(test)]
 mod conn_params_tests {
@@ -85,23 +130,46 @@ mod conn_params_tests {
 
     #[test]
     fn test_new() {
-        let user = "test";
-        let password = "test";
         let host: (u8, u8, u8, u8) = (0, 0, 0, 0); 
-        let port = 8000;
-        let database = "test";
         
         let conn_params = ConnParamsBuilder::new()
-            .user(user)
-            .password(Some(password))
+            .user("test")
+            .password("test")
             .host(host)
-            .port(port)
-            .database(database)
+            .port(8000)
+            .database("test")
             .build();
 
         let conn_params_default = ConnParamsBuilder::new()
             .build();
 
-        assert_ne(conn_params.)
+        assert_ne!(conn_params.user(), conn_params_default.user());
+        assert_ne!(conn_params.password(), conn_params_default.password());
+        assert_ne!(conn_params.host(), conn_params_default.host());
+        assert_ne!(conn_params.port(), conn_params_default.port());
+        assert_ne!(conn_params.database(), conn_params_default.database());
+    }
+
+    #[test]
+    fn test_to_string_default() {
+        let conn_params = ConnParamsBuilder::new()
+            .build();
+
+        assert_eq!(
+            conn_params.to_string(),
+            "postgresql://postgres@127.0.0.1:5432/postgres".to_string(),
+        );
+    }
+
+    #[test]
+    fn test_to_string_with_password() {
+        let conn_params = ConnParamsBuilder::new()
+            .password("test")
+            .build();
+    
+        assert_eq!(
+            conn_params.to_string(),
+            "postgresql://postgres:test@127.0.0.1:5432/postgres".to_string(),
+        );
     }
 }
--- src/protocol/connection.rs
+++ src/protocol/connection.rs
@@ -5,11 +5,11 @@ pub struct Connection<'a> {
     url: &'a String,
     connection: TcpListener,
 }
-
-
-impl<'a> Connection<'a> {
-    pub fn connect(url: &'a str) -> Self {
-        Connection { url }
-    }   
-}
+//
+//
+// impl<'a> Connection<'a> {
+//     pub fn connect(url: &'a str) -> Self {
+//         Connection { url }
+//     }   
+// }
 
